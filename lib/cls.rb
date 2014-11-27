f801d4cf52357df3a6ec2c5688c1a5ffff138da5
Amir Kremer <amir.kremer@klarna.com>
2014-11-27 15:02:44 +0200

    add optional keyword arguments

--- lib/cls.rb
+++ lib/cls.rb
@@ -1,15 +1,14 @@
 module Cls
   VERSION = "0.0.3"
 
-  def takes(*args)
-    keyword_args = args[-1].is_a?(Hash) ? args.delete(args[-1]) : {}
-    required_keyword_args = keyword_args[:required] || []
-    define_initialize(args, required_keyword_args)
+  def takes(*args, **kwargs)
+    define_initialize(args, kwargs[:required] || [], kwargs[:optional] || {})
   end
 
-  def define_initialize(args, required_keyword_args)
-    assignments = (args + required_keyword_args).map { |a| "@#{a} = #{a}" }.join("\n")
-    arguments = args + required_keyword_args.map { |a| "#{a.to_s}:" }
+  def define_initialize(args, required_keyword_args, optional_keyword_args)
+    arguments = args + required_keyword_args.map{ |a| "#{a.to_s}:" } +
+                       optional_keyword_args.collect { |a, value| "#{a.to_s}: #{value.inspect}"}
+    assignments = (args + required_keyword_args + optional_keyword_args.keys).map { |a| "@#{a} = #{a}" }.join("\n")
 
     self.class_eval %{
       def initialize(#{arguments.join(", ")})
